8af6f69218bce7a93fd5e6b14f50c5e5e9226bef
Michael Usner <michael.usner@hpe.com>
2018-02-11 08:18:57 -0600

    wip

--- src/musner/VirtualEnv.groovy
+++ src/musner/VirtualEnv.groovy
@@ -5,14 +5,10 @@ class VirtualEnv {
 
     VirtualEnv(name) { 
         this.name = name
-        if (!this.isWindows()) {
-            
-            println("Created new virtualenv ${name}")
-        }
-    }
-
-    def isWindows() {
-        return System.properties['os.name'].toLowerCase().contains('windows')
+        println("Created new virtualenv ${name}")
     }
 }
 
+def isWindows() {
+    return System.properties['os.name'].toLowerCase().contains('windows')
+}
