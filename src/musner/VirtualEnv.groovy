1b88564f8836469ce7fdd8b8728e0572d5bd733b
Michael Usner <michael.usner@hpe.com>
2018-02-11 07:55:41 -0600

    wip

--- src/musner/VirtualEnv.groovy
+++ src/musner/VirtualEnv.groovy
@@ -5,8 +5,14 @@ class VirtualEnv {
 
     VirtualEnv(name) { 
         this.name = name
-        sh
-        println("Created new virtualenv ${name}")
+        if (!this.isWindows()) {
+            def sout = new StringBuilder(), serr = new StringBuilder()
+            def proc = 'virtualenv ${name}'.execute()
+            proc.consumeProcessOutput(sout, serr)
+            proc.waitForOrKill(1000)
+            println "out> $sout err> $serr"
+            println("Created new virtualenv ${name}")
+        }
     }
 
     def isWindows() {
