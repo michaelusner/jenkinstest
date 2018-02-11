0ef5d8730091e751cea91e905fc0f828d67a24ab
Michael Usner <michael.usner@hpe.com>
2018-02-11 11:15:36 -0600

    wip

---
+++ vars/venv.groovy
@@ -0,0 +1,29 @@
+def withLinux(args) {
+    // see if virtualenv is installed
+    res = sh(returnStatus: true,
+        script: """
+            if ! pip freeze | grep virtualenv; then
+                python2 -m pip install virtualenv
+            fi
+        """
+    )
+    if (res != 0) {
+        error("Failed to install virtualenv")
+    }
+
+    // create the env if it doesn't exist
+    res = sh(returnStatus: true,
+        script: """
+            if [ ! -d "py27" ]; then
+                python2 -m virtualenv py27           
+            fi
+            . ./py27/bin/activate
+            ${args}
+        """
+    )
+    println "result: ${res}"
+    if (res != 0) {
+        error("Failed to run command")
+    }
+    return res
+}
